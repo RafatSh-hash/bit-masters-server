84aa50e82dcfde98c42dc5334e689ddf6a9c9f0a
RafatSh-hash <73437594+RafatSh-hash@users.noreply.github.com>
2022-10-26 18:13:11 +0600

    modified data

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const courses = require("./data/courses.json");
 
 app.use(cors());
 
+const courses = require("./data/courses.json");
+const catagories = require("./data/courseCategory.json");
+
 app.get("/", (req, res) => {
   res.send("Courses API running successfully");
+});
+
+app.get("/catagories", (req, res) => {
+  res.send(catagories);
+});
+
+app.get("/category/:CatID", (req, res) => {
+  const id = req.params.CatID;
+
+  const categoryCourses = courses.filter((c) => c.CatID === id);
+  console.log(categoryCourses);
+  res.send(categoryCourses);
 });
 
 app.get("/courses", (req, res) => {
